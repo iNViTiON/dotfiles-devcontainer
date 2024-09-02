07891409521cf7504a141fcc325d3bff311e0f3d
MiX <13241662+iNViTiON@users.noreply.github.com>
2024-09-02 16:06:42 +0300

    chore: only use official Volta
    Volta 2 now have prebuilt ARM version for Linux.

--- volta-setup.sh
+++ volta-setup.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 set -e
 
-if [[ $(uname -m) == arm* || $(uname -m) == aarch* ]]; then
-  mkdir -p ~/.volta
-  cd ~/.volta
-  curl -L $(curl -sX GET https://api.github.com/repos/MyneTop/volta/releases/latest | grep browser_download_url | cut -d : -f 2,3 | tr -d '"[:blank:]') | tar -xzvf -
-  ./volta setup
-  cd -
-else
-  curl https://get.volta.sh | bash
-fi
+curl https://get.volta.sh | bash
 
 if ! grep -qF 'export PATH="$VOLTA_HOME:$PATH"' ~/.bashrc; then
     echo 'export PATH="$VOLTA_HOME:$PATH"' >> ~/.bashrc
